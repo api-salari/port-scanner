1d8a0d3c1860fa84d8192982250116234ce21f7c
api-salari <147167298+api-salari@users.noreply.github.com>
2023-12-08 03:47:51 -0800

    Update port-scaner.py

--- scanner-port.py
+++ scanner-port.py
@@ -1,102 +1,90 @@
-import sys, platform
-import socket
-import time
-import threading
-import requests
-from queue import Queue
-gn = '\033[00;32m'
-rd = '\033[00;31m'
-yw = '\033[00;33m'
-Lgn = '\033[01;32m'
-be = '\033[00;34m'
-we = '\033[01;37m'
-
-banner = f"""{yw}
-------------------------------------------------------------
-{gn}@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@&BPY??????J5G#@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@BY~:.:^!.   !!^..:!Y#@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@&5^ .75B&@#. . 7@@&B5!. ~P@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@P: ~P&@@@@&^ !#: 7@@@@@&5: ^G@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@? .5@@@@@@&~ ^&@G  J@@@@@@@Y  J@@@@@@@@@@@@@@
-{we}@@@@@@@@@@@@@@7 :B@@@@@@@! :#@@@5  Y@@@@@@@G  J@@@@@@@@@@@@@
-@@@@@@@@@@@@@5  G@@@@@@@? .B@@@@@J  P@@@@@@@5  B@@@@@@@@@@@@
-@@@@@@@@@@@@@! :PPPPPPP?  ~PPPPPPP: .5PPPPPP5. ?@@@@@@@@@@@@
-@@@@@@@@@@@@@~ .::::::   :^:::::::^.  .::::::  ?@@@@@@@@@@@@
-@@@@@@@@@@@@@? :&@@@@P  !@@@@@@@@@@B. :#@@@@B  Y@@@@@@@@@@@@
-@@@@@@@@@@@@@B. ?@@@B. ^&@@@@@@@@@@@P  ~&@@@! :&@@@@@@@@@@@@
-@@@@@@@@@@@@@@P  J@#: .#@@@@@@@@@@@@@J  !@@7 .B@@@@@@@@@@@@@
-{rd}@@@@@@@@@@@@@@@G: ~^ .G@@@@@@@@@@@@@@@7  !^ ~#@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@&J.  ^&@@@@@@@@@@@@@@@B.  :5@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@&5!..^?5G##&&##BPY7^.:!P@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@#P?~:...::...:^!JG&@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@&#BBBBB#&@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-{yw}------------------------------------------------------------
-"""
-for c in banner:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.005)
-
-print ("")
-print ("")
-print ("")
-
-IP = requests.get('https://get.geojs.io/v1/ip.json')
-myip = IP.json()['ip']
-print(f"{rd}[*]{yw} You ip : {gn}"+myip)
-
-print(f'{rd}[*]{yw} version  :{gn}', platform.version())
-socket.setdefaulttimeout(0.25)
-lock = threading.Lock()
-print (f"{be}____________________________________")
-time.sleep(4)
-print (f"""
-{rd}[-]{yw} Creator channel Rubika: {Lgn}@tyro_code
-{rd}[!]{yw}To use, please enter https and http! 
-{rd}[!] Communication is TCP! 
-""")
-print ("")
-time.sleep(2)
-host = input(f"{rd} ┌─["+f"{Lgn}@tyro_code"+f"{be}/"+f"{we}port-scanner"+f"""{rd}]
- └──╼ """+f"{gn}Enter the site address : >>>{yw} ")
-print ("")
-time.sleep(1)
-print (f'{rd}[+]{yw} IP Address : {gn}', host)
-print (f"{be}____________________________________")
-def scanner(port):
-   tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   try:
-      Connect = tcpsocket.connect((host, port))
-          
-      with lock:
-         time.sleep(2)
-         print(f'{rd}[!]{yw} Port :{Lgn}', port, f'{yw} is open! ')
-
-         print(f"{be}____________________________________")
-      Connect.close()
-   except:
-      pass
-
-def execute():
-   while True:
-      worker = queue.get()
-      scanner(worker)
-      queue.task_done()
-      
-queue = Queue()
-start_time = time.time()
-   
-for x in range(100):
-   thread = threading.Thread(target = execute)
-   thread.daemon = True
-   thread.start()
-   
-for worker in range(1, 500):
-   queue.put(worker)
-   
-queue.join()
-
-print (f"{rd}[*]{gn} channel Rubika : {Lgn} https://rubika.ir/tyro_code")
+from os import system
+try:
+    from sys import argv
+except:
+    system('pip install sys')
+try:
+    import socket 
+except:
+    system("pip install socket")
+class Fore:
+    red = "\033[0;31m"
+    green = "\033[0;32m"
+    yellow = "\033[0;33m"
+    blue = "\033[0;34m"
+    cyan = "\033[0;36m"
+    white = "\033[0;37m"
+    nc = "\033[00m"
+try:
+    system("cls")
+except:
+    system("clear")
+print(Fore.green,
+        """
+                                                    ..::^^~!!7??JJYY
+                                    ...::^^~!!7??JJYYY555555555555Y
+                        ..::^^~~!!7~  ~YYY555555555555555555555555555Y
+            :^^~!!77?JJJYYY55555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            55555555555555555555555?  !5555555555555555555555555555555
+            YYYYYYYYYYYYYYJJYYJJJJY7  ~YJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ?
+            ........................                                  
+            JJJJJJJJJJJJJJJJJJJJJJJ7  ~J??????????????????????????????
+            55555555555555555555555?  !5555555555555555555555555555555
+            55555555555555555555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  !555555555555555555555555555555Y
+            55555555555555555555555?  ~555555555555555555555555555555Y
+            ^^~!!77??JJYYY555555555?  !555555555555555555555555555555Y
+                    ...::^^~!!7?!  ~YYY555555555555555555555555555Y
+                                    ...::^^~!!77??JJYYY55555555555Y
+                                                    ...::^~~!77?JJYY
+""",
+    )
+def scan(target, start_port, end_port):
+    print('\n\n')
+    listtrue = []
+    for port in range(int(start_port), int(end_port) + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            print(Fore.white, "--" * 10)
+            print(Fore.green, f"Port {port} is open")
+            listtrue.append(port)
+            sock.close()
+        else:
+            print(Fore.white, "--" * 10)
+            print(Fore.red, f"Port {port} not open")
+    if listtrue != []:
+        print(Fore.blue, "--" * 10, "\n\n\n")
+        for x in listtrue:
+            print(Fore.green, f"port in {x} open")
+    print(Fore.white)
+if argv[1] == '-i':
+    try:
+        scan(target=argv[2], start_port=argv[3], end_port=argv[4])
+    except:
+        target = input(f"{Fore.green}[+]{Fore.white} Enter ip target : ")
+        start_port = int(input(f"{Fore.green}[+]{Fore.white} Enter Yor start port : "))
+        end_port= int(input(f"{Fore.green}[+]{Fore.white} Enter Yor end port : "))
+        scan(target=target, start_port=start_port, end_port=end_port)
+elif argv[1] == '-s':
+    try:
+        ip_addr = socket.gethostbyname(argv[2])
+        print(Fore.green+"[+]"+Fore.white+" ip "+argv[2]+' : '+ip_addr)
+        scan(target=ip_addr, start_port=argv[3], end_port=argv[4])
+    except:
+        target = input(f"{Fore.green}[+]{Fore.white} Enter ip target : ")
+        start_port = int(input(f"{Fore.green}[+]{Fore.white} Enter Yor start port : "))
+        end_port = int(input(f"{Fore.green}[+]{Fore.white} Enter Yor end port : "))
+        scan(target=target, start_port=start_port, end_port=end_port)
